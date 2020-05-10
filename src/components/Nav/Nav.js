13c8862b6b6f3e12c0f72870a7cce92cc0c07005
Gino <ginohagad@gmail.com>
2020-05-11 11:34:42 +1200

    Pending: menu icon to show up only for mobile.

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from './Nav.module.css'
 import { makeStyles } from '@material-ui/core/styles'
-import { Tabs, Tab, AppBar, Toolbar, Typography, Button, IconButton } from '@material-ui/core'
+import { Tabs, Tab, AppBar, Toolbar, Typography, Button, IconButton, ClickAwayListener, Grow, Paper, Popper, MenuItem, MenuList } from '@material-ui/core'
 import { Link } from 'react-router-dom'
 import MenuIcon from '@material-ui/icons/Menu'
 
@@ -17,27 +17,91 @@ const useStyles = makeStyles((theme) => ({
 	title: {
 		flexGrow: 1,
 	},
+	paper: {
+		marginRight: theme.spacing(2),
+	},
 }))
 
 
 const Nav = () => {
 	const classes = useStyles()
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef(null);
+
+  const handleToggle = () => {
+			setOpen((prevOpen) => !prevOpen);
+	};
+
+  const handleClose = (event) => {
+		if (anchorRef.current && anchorRef.current.contains(event.target)) {
+			return;
+		}
+
+		setOpen(false);
+	};
+
+  function handleListKeyDown(event) {
+		if (event.key === 'Tab') {
+			event.preventDefault();
+			setOpen(false);
+		}
+  }
+
+	// return focus to the button when we transition from !open to open
+  const prevOpen = React.useRef(open);
+  React.useEffect(() => {
+		if (prevOpen.current === true && open === false) {
+			anchorRef.current.focus();
+		}
+	
+		prevOpen.current = open;
+	}, [open]);
 
 	return (
 		<div className={classes.root}>
 			<AppBar position="static" style={{ background: 'transparent', boxShadow: 'none'}}>
 				<Toolbar>
-					<IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+					<IconButton 
+						edge="start" 
+						className={classes.menuButton} 
+						color="inherit" 
+						aria-label="menu" 
+						ref={anchorRef} 
+						aria-controls={open ? 'menu-list-grow' : undefined}
+						aria-haspopup="true"	
+						onClick={handleToggle}
+					>
 						<MenuIcon />	
 					</IconButton>
+					<Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal>
+						{({ TransitionProps, placement }) => (
+							<Grow
+								{...TransitionProps}
+								style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
+							>
+								<Paper>
+									<ClickAwayListener onClickAway={handleClose}>
+										<MenuList autoFocusItem={open} id="menu-list-grow" onKeyDown={handleListKeyDown}>
+											<MenuItem onClick={handleClose} to='/' component={Link}>Home</MenuItem>
+											<MenuItem onClick={handleClose} to='our-food' component={Link}>Our Food</MenuItem>
+											<MenuItem onClick={handleClose}	to='gallery'component={Link}>Gallery</MenuItem>
+											<MenuItem onClick={handleClose}	to='blog' component={Link}>Blog</MenuItem>
+											<MenuItem onClick={handleClose}	to='order' compnent={Link}>Order Now</MenuItem>
+											<MenuItem onClick={handleClose}	to='location' component={Link}>Location</MenuItem>
+										</MenuList>
+									</ClickAwayListener>
+								</Paper>
+							</Grow>
+						)}
+					</Popper>
 					<Typography variant="h6" className={classes.title}>Bistro</Typography>
 					<Tabs>
 						<Tab label='Home' to='/' component={Link} />} />
-						<Tab label='Our Food' to='/our-food' component={Link} />
-						<Tab label='Gallery' to='/gallery' component={Link} />
-						<Tab label='Blog' to='/blog' component={Link} />
-						<Tab label='Order Now' to='/order' component={Link} />
-						<Tab label='Location' to='/location' component={Link} />
+						<Tab label='Our Food' to='our-food' component={Link} />
+						<Tab label='Gallery' to='gallery' component={Link} />
+						<Tab label='Blog' to='blog' component={Link} />
+						<Tab label='Order Now' to='order' component={Link} />
+						<Tab label='Location' to='location' component={Link} />
 					</Tabs>
 			</Toolbar>
 			</AppBar>
