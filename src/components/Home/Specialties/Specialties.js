88a52542b4beaa3cdfbb951713535680d476ce51
Gino <ginohagad@gmail.com>
2020-05-15 16:53:32 +1200

    Fixing margin on Specialties section.

--- src/components/Home/Specialties/Specialties.js
+++ src/components/Home/Specialties/Specialties.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './Specialties.module.css'
 import { makeStyles } from '@material-ui/core/styles'
+
 import FreeBreakfastIcon from '@material-ui/icons/FreeBreakfast';
 import LocalBarIcon from '@material-ui/icons/LocalBar';
 import FastfoodIcon from '@material-ui/icons/Fastfood';
@@ -9,9 +10,8 @@ import LocalDrinkIcon from '@material-ui/icons/LocalDrink';
 
 const useStyles = makeStyles((theme) => ({
   icons: {
-    display: 'inline-flex',
-    verticalAlign: 'middle !important',
-  },
+    fontSize: '40px',
+  }
 }));
 
 
@@ -24,19 +24,22 @@ const Specialties = () => {
       <h1 className={`${styles.title} ${styles.center}`}>Our Specialties</h1>
       <div className={styles.boxes}>
         <div className={styles.box}>
-          <FreeBreakfastIcon className={classes.icons} fontSize="large" />
-          <h3 className={styles.center}>Dinner & Dessert</h3>
-          <p className={styles.center}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, eaque sunt. Nisi vel illum adipisci.</p>
-        </div>
-        <div className={styles.box}>
+          <FreeBreakfastIcon className={classes.icons} />
           <h3 className={styles.center}>Breakfast</h3>
           <p className={styles.center}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, eaque sunt. Nisi vel illum adipisci.</p>
         </div>
         <div className={styles.box}>
+          <LocalBarIcon className={classes.icons} />
+          <h3 className={styles.center}>Dinner & Dessert</h3>
+          <p className={styles.center}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, eaque sunt. Nisi vel illum adipisci.</p>
+        </div>
+        <div className={styles.box}>
+          <FastfoodIcon className={classes.icons} />
           <h3 className={styles.center}>Ice Shakes</h3>
           <p className={styles.center}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, eaque sunt. Nisi vel illum adipisci.</p>
         </div>
         <div className={styles.box}>
+          <LocalDrinkIcon className={classes.icons} />
           <h3 className={styles.center}>Beverages</h3>
           <p className={styles.center}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, eaque sunt. Nisi vel illum adipisci.</p>
         </div>
