c91fa249edb977c24f142af404cf81b1169d35dd
Gino <ginohagad@gmail.com>
2020-06-27 09:34:20 +1200

    Featured Food section: Contents for each cards are now mapped from a 'Food' array.

--- src/components/Home/FeaturedFood/FeaturedFood.js
+++ src/components/Home/FeaturedFood/FeaturedFood.js
@@ -2,6 +2,10 @@ import React from 'react'
 import { Typography, Card, CardMedia, CardContent, Grid } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 import styles from './FeaturedFood.module.css'
+import pizza from '../../../img/food/pizza.jpg'
+import salad from '../../../img/food/salad.jpg'
+import soup from '../../../img/food/soup.jpg'
+import pasta from '../../../img/food/pasta.jpg'
 
 
 
@@ -23,82 +27,58 @@ const useStyles = makeStyles((theme) => ({
 
 const FeaturedFood = () => {
 
+	const food = [
+		{
+			src: pizza,
+			name: 'Pizza',
+			desc: 'This is a pizza',
+		},
+		{
+			src: salad,
+			name: 'Salad',
+			desc: 'This is a salad',
+		},
+		{
+			src: soup,
+			name: 'Soup',
+			desc: 'This is a soup',
+		},
+		{
+			src: pasta,
+			name: 'Pasta',
+			desc: 'This is a pasta',
+		},
+	];
+
+
+
+
   const classes = useStyles()
 
   return (
     <div className={styles.container}>
       <Typography variant="h4" className={styles.title}>Featured Food</Typography>
       <hr className={styles.line} />
-      <Grid container spacing={24} alignItems="center" justify="center" className={classes.gridContainer}>
-        <Grid item md={3}>
-          <Card className={classes.card}>
-            <CardMedia
-              className={classes.media}
-              image="https://placeimg.com/640/480/any"
-            />
-            <CardContent>
-              <Typography variant="h6">
-                Food name 
-              </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-                This impressive paella is a perfect party dish and a fun meal to cook together with your
-                guests. Add 1 cup of frozen peas along with the mussels, if you like.
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item md={3}>
-          <Card className={classes.card}>
-            <CardMedia
-              className={classes.media}
-              image="https://placeimg.com/640/480/any"
-            />
-            <CardContent>
-              <Typography variant="h6">
-                Food name 
-              </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-                This impressive paella is a perfect party dish and a fun meal to cook together with your
-                guests. Add 1 cup of frozen peas along with the mussels, if you like.
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item md={3}>
-          <Card className={classes.card}>
-            <CardMedia
-              className={classes.media}
-              image="https://placeimg.com/640/480/any"
-            />
-            <CardContent>
-              <Typography variant="h6">
-                Food name 
-              </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-                This impressive paella is a perfect party dish and a fun meal to cook together with your
-                guests. Add 1 cup of frozen peas along with the mussels, if you like.
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item md={3}>
-          <Card className={classes.card}>
-            <CardMedia
-              className={classes.media}
-              image="https://placeimg.com/640/480/any"
-            />
-            <CardContent>
-              <Typography variant="h6">
-                Food name 
-              </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-                This impressive paella is a perfect party dish and a fun meal to cook together with your
-                guests. Add 1 cup of frozen peas along with the mussels, if you like.
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-      </Grid>
+			<Grid container spacing={24} alignItems="center" justify="center" className={classes.gridContainer}>
+				{food.map((f, idx) => 
+					<Grid key={idx} item md={3}>
+						<Card className={classes.card}>
+							<CardMedia
+								className={classes.media}
+								image={f.src}
+							/>
+							<CardContent>
+								<Typography variant="h6">
+									{f.name}
+								</Typography>
+								<Typography variant="body2" color="textSecondary" component="p">
+									{f.desc}
+								</Typography>
+							</CardContent>
+						</Card>
+					</Grid>
+				)}
+			</Grid>
     </div>
   )
 }
